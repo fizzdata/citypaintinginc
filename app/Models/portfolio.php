5612b653ebb2022f4b6a6856f7353092a4bebcad
Yoely@peneh <yoely@peneh.com>
2026-02-10 21:16:35 -0500

    fixed projects

--- app/Models/portfolio.php
+++ app/Models/portfolio.php
@@ -39,18 +39,18 @@ class portfolio extends Model
                 'slide' => 'slideInLeft',
             ],
             [
-                'folder_name' => '286 rider ave',
-                'inside_images' => ['286.png'],
-                'outside_img' => '286.jpg',
+                'folder_name' => '286-rider-ave',
+                'inside_images' => array_map('basename', glob(public_path('images/portfolio/286-rider-ave') . '/*')),
+                'outside_img' => 'outside.jpg',
                 'text1' => '286 Rider Ave, Bronx, NY 10451',
                 'text2' => 'Painting, and wallcovering',
                 'slide' => 'slideInLeft',
             ],
 
             [
-                'folder_name' => '322 grand concourse',
-                'inside_images' => ['322.jpg'],
-                'outside_img' => '322.jpg',
+                'folder_name' => '322-grand-concourse',
+                'inside_images' => array_map('basename', glob(public_path('images/portfolio/322-grand-concourse') . '/*')),
+                'outside_img' => 'outside.jpg',
                 'text1' => '322 Grand Concourse, Bronx, NY 10451',
                 'text2' => 'Taping, Kedex, Primer, Panting, Wallpaper, Epoxy Painting',
                 'slide' => 'slideInUp',
@@ -58,9 +58,9 @@ class portfolio extends Model
             ],
 
             [
-                'folder_name' => '130 hope st',
-                'inside_images' => ['130.jpg'],
-                'outside_img' => '130.jpg',
+                'folder_name' => '130-hope-st',
+                'inside_images' => array_map('basename', glob(public_path('images/portfolio/130-hope-st') . '/*')),
+                'outside_img' => 'outside.jpg',
                 'text1' => '130 Hope St, Brooklyn, NY 11211',
                 'text2' => 'Taping, Kedex, Primer, Panting, Wallpaper, Epoxy Painting',
                 'slide' => 'slideInRight',
@@ -68,9 +68,9 @@ class portfolio extends Model
                           
 
             [
-                'folder_name' => '540 waverly ave',
-                'inside_images' => ['540.jpg'],
-                'outside_img' => '540.jpg',
+                'folder_name' => '540-waverly-ave',
+                'inside_images' => array_map('basename', glob(public_path('images/portfolio/540-waverly-ave') . '/*')),
+                'outside_img' => 'outside.jpg',
                 'text1' => '540 Waverly Ave, Brooklyn, NY 11238',
                 'text2' => 'Taping, Kedex, Primer, Panting, Wallpaper, Epoxy Painting',
                 'slide' => 'slideInUp',
@@ -78,9 +78,9 @@ class portfolio extends Model
             ],
 
             [
-                'folder_name' => '210 clarkson ave',
-                'inside_images' => ['210.jpg'],
-                'outside_img' => '210.jpg',
+                'folder_name' => '210-clarkson-ave',
+                'inside_images' => array_map('basename', glob(public_path('images/portfolio/210-clarkson-ave') . '/*')),
+                'outside_img' => 'outside.jpg',
                 'text1' => '210 Clarkson Ave Brooklyn, NY 11226',
                 'text2' => 'Taping, Kedex, Primer, Panting, Wallpaper, Epoxy Painting',
                 'slide' => 'slideInLeft',
@@ -88,9 +88,9 @@ class portfolio extends Model
             ],
 
             [
-                'folder_name' => '219 hudson street',
-                'inside_images' => ['219.webp'],
-                'outside_img' => '219.jpg',
+                'folder_name' => '219-hudson-street',
+                'inside_images' => array_map('basename', glob(public_path('images/portfolio/219-hudson-street') . '/*')),
+                'outside_img' => 'outside.jpg',
                 'text1' => '219 Hudson Street, New York, NY, 10013',
                 'text2' => 'Taping, Kedex, Primer, Panting, Wallpaper, Epoxy Painting',
                 'slide' => 'slideInRight',
@@ -98,9 +98,9 @@ class portfolio extends Model
             ],
 
             [
-                'folder_name' => '237 mcguinness blvd',
-                'inside_images' => ['237.jpg'],
-                'outside_img' => '237.jpg',
+                'folder_name' => '237-mcguinness-blvd',
+                'inside_images' => array_map('basename', glob(public_path('images/portfolio/237-mcguinness-blvd') . '/*')),
+                'outside_img' => 'outside.jpg',
                 'text1' => '237 mcGuinness blvd brooklyn ny 11222',
                 'text2' => ' Taping, Painting',
                 'slide' => 'slideInDown',
@@ -108,9 +108,9 @@ class portfolio extends Model
             ],
 
             [
-                'folder_name' => '10 montieth street',
-                'inside_images' => ['10.jpg'],
-                'outside_img' => '10.jpg',
+                'folder_name' => '10-montieth-street',
+                'inside_images' => array_map('basename', glob(public_path('images/portfolio/10-montieth-street') . '/*')),
+                'outside_img' => 'outside.jpg',
                 'text1' => '10 MONTIETH STREET BROOKLYN, NY 11206',
                 'text2' => 'Taping, Kedex, Primer, Panting, Wallpaper, Epoxy Painting',
                 'slide' => 'slideInDown',
@@ -118,9 +118,9 @@ class portfolio extends Model
             ],
 
             [
-                'folder_name' => '1000 westchester ave',
-                'inside_images' => ['1000.jpg','1000.jpg'],
-                'outside_img' => '1000.jpg',
+                'folder_name' => '1000-westchester-ave',
+                'inside_images' => array_map('basename', glob(public_path('images/portfolio/1000-westchester-ave') . '/*')),
+                'outside_img' => 'outside.jpg',
                 'text1' => '1000 Westchester Ave Bronx, NY 10459',
                 'text2' => 'Panting, Epoxy Painting',
                 'slide' => 'slideInLeft',
@@ -129,7 +129,7 @@ class portfolio extends Model
 
             [
                 'folder_name' => '44-01-northern-blvd',
-                'inside_images' => ['44-01.jpg'],
+                'inside_images' => array_map('basename', glob(public_path('images/portfolio/44-01-northern-blvd') . '/*')),
                 'outside_img' => 'outside.jpg',
                 'text1' => '44-01 Northern Blvd Long Island City, NY 11101',
                 'text2' => 'Painting, Epoxy Paint, Venetian Plaster, & Wallcovering',
